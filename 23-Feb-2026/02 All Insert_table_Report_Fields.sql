be6fc5a1b9fcc52f396e0f0cf1d6f980ced8dd4e
HelpdeskRegulusit <helpdesk@regulusit.net>
2026-02-24 09:58:23 +0530

    DB changes for tasks - #3582 - Datewise fee collection report 3543 - VP - Design Pre-Primary Term-II report.(Final Report Card) #3580-SNS- Unable to generate biometric staff attendance report

---
+++ 23-Feb-2026/02 All Insert_table_Report_Fields.sql
@@ -0,0 +1,49 @@
+INSERT INTO [dbo].[Report_Fields]
+           ([Report_Field_Id]
+           ,[Field_name]
+           ,[Display_name]
+           ,[Data_type]
+           ,[Display_Filter_Values]
+           ,[View_Name_For_Filter_values]
+           ,[OrderBYColumn]
+           ,[Is_Deleted]
+           ,[Is_Dependent]
+           ,[Parent_Field_Id]
+           ,[Filter_Field_Name]
+           ,[Is_Parent]
+           ,[Is_Report_Filter_Field]
+           ,[Additional_Parent_Field_Id]
+           ,[Additional_Filter_Field_Name])
+     VALUES
+           (1176,
+		   '{usp_GetDetailsForPrePrimaryReport;1.Term_Id}',
+		    'Term',
+			'DropDownList',
+			 1,
+			'vw_studentTermwiseTestMaster',
+			'Term_Id',
+			'N',
+			'N',
+		    1176,
+		    '{usp_GetDetailsForPrePrimaryReport;1.Term_Id}',
+			'N',
+			'Y',
+			NULL,
+			NULL)	
+			
+
+			INSERT INTO [dbo].[Report_Field_Selection]
+           ([Report_Field_Selection_Id]
+           ,[Report_Id]
+           ,[Report_Field_Id]
+           ,[Display_Order]
+           ,[Is_Requried]
+           ,[Is_Deleted])
+     VALUES
+           (1176,
+		    321,
+			1176,
+			4,
+			'Y',
+			'N'
+			)
---
+++ 23-Feb-2026/06 All Update_table_reports.sql
@@ -0,0 +1,4 @@
+update reports
+set Report_Display_Name = 'Student Pre-Primary Report'
+where Report_Display_Name like '%Term-I Pre-Primary Report%'
+and Report_Id = 321
