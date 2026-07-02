eaf41d71a884b9ad3146fdbdd4344a7d80da7fd1
RIT Sachin <helpdesk@regulusit.net>
2026-07-02 09:57:09 +0530

    DB change for - TPS - Fees configuration changes for 1st to 8th std PPSH - Pending fee report for all years PPS - Fee screen - Slowness issue #SNS - Internal fee - It is not adding value for account header id 3753-SNS- Update Stream Wise Subject Details for AY 2026

---
+++ 30-Jun-2026/02 TPS Insert_table_StandardwiseFeeInstallmentDetails.sql
@@ -0,0 +1,103 @@
+UPDATE SSFTM
+SET Interval = 11
+from Schoolwise_Standard_FeeType_Master SSFTM
+INNER JOIN Schoolwise_Fee_Type_Configuration SFTC
+ON SSFTM.Fee_Type_Id = SFTC.Fee_Type_Id
+INNER JOIN Standard_Master SM
+ON SSFTM.Standard_Id = SM.Standard_Id
+WHERE SFTC.School_Id = 172
+AND SFTC.Academic_Year_Id = 1
+AND SFTC.Is_Deleted = 'N'
+AND SSFTM.Is_Deleted = 'N'
+AND SFTC.Fee_Type = 'Tuition Fees'
+AND SM.Is_Deleted = 'N'
+AND SM.Standard_Name IN ('1','2','3','4','5','6','7','8')
+
+-----------------
+UPDATE SSLDD
+SET IsDeleted = 1,
+UpdatedById = 1,
+UpdatedDate = DBO.GetLocalDate(DEFAULT)
+from SchoolWise_Standard_LateFee_DueDates_Master SSLDM
+INNER JOIN Schoolwise_Standard_FeeType_Master SSFTM
+ON SSLDM.SchoolWise_Standard_FeeType_Id = SSFTM.SchoolWise_Standard_FeeType_Id
+INNER JOIN Schoolwise_Fee_Type_Configuration SFTC
+ON SSFTM.Fee_Type_Id = SFTC.Fee_Type_Id
+INNER JOIN Standard_Master SM
+ON SSFTM.Standard_Id = SM.Standard_Id
+INNER JOIN SchoolWise_Standard_LateFee_DueDates_Details SSLDD
+ON SSLDM.SchoolWise_Standard_LateFee_DueDates_Id = SSLDD.SchoolWise_Standard_LateFee_DueDates_Id
+WHERE SFTC.School_Id = 172
+AND SFTC.Academic_Year_Id = 1
+AND SFTC.Is_Deleted = 'N'
+AND SSFTM.Is_Deleted = 'N'
+AND SM.Is_Deleted = 'N'
+AND SSLDM.Is_Deleted = 'N'
+AND SSLDD.IsDeleted = 0
+AND SM.Standard_Name IN ('1','2','3','4','5','6','7','8')
+
+UPDATE SSLDM
+SET Is_Deleted = 'Y',
+Updated_By_Id = 1,
+Update_Date = DBO.GetLocalDate(DEFAULT)
+from SchoolWise_Standard_LateFee_DueDates_Master SSLDM
+INNER JOIN Schoolwise_Standard_FeeType_Master SSFTM
+ON SSLDM.SchoolWise_Standard_FeeType_Id = SSFTM.SchoolWise_Standard_FeeType_Id
+INNER JOIN Schoolwise_Fee_Type_Configuration SFTC
+ON SSFTM.Fee_Type_Id = SFTC.Fee_Type_Id
+INNER JOIN Standard_Master SM
+ON SSFTM.Standard_Id = SM.Standard_Id
+WHERE SFTC.School_Id = 172
+AND SFTC.Academic_Year_Id = 1
+AND SFTC.Is_Deleted = 'N'
+AND SSFTM.Is_Deleted = 'N'
+AND SM.Is_Deleted = 'N'
+AND SSLDM.Is_Deleted = 'N'
+AND SM.Standard_Name IN ('1','2','3','4','5','6','7','8')
+---------------------------
+UPDATE SSFCD
+SET Fee_AmountOld = 4500,
+	Fee_AmountNew = 4500,
+	Update_Date = DBO.GetLocalDate(DEFAULT),
+	Updated_By_Id = 1
+from Schoolwise_Fee_Type_Configuration SFTC
+INNER JOIN Schoolwise_Standard_Fee_Configuration_Master SSFCM
+ON SFTC.Fee_Type_Id = SSFCM.Fee_Type_Id
+INNER JOIN Schoolwise_Standard_Fee_Configuration_Details SSFCD
+ON SSFCM.Schoolwise_Standard_Fee_Configuration_Id = SSFCD.Schoolwise_Standard_Fee_Configuration_Id
+INNER JOIN Standard_Master SM
+ON SSFCM.Standard_Id = SM.Standard_Id
+WHERE SFTC.School_Id = 172
+AND SFTC.Academic_Year_Id = 1
+AND SFTC.Is_Deleted = 'N'
+AND SSFCM.Is_Deleted = 'N'
+AND SFTC.Fee_Type = 'Tuition Fees'
+AND SM.Is_Deleted = 'N'
+AND SSFCD.Is_Deleted = 'N'
+AND SM.Standard_Name IN ('1','2','3','4','5','6','7','8')
+
+UPDATE SSFCM
+SET OldStudent_TotalFee = 4500,
+	NewStudent_TotalFee = 4500,
+	Update_Date = DBO.GetLocalDate(DEFAULT),
+	Updated_By_Id = 1
+from Schoolwise_Fee_Type_Configuration SFTC
+INNER JOIN Schoolwise_Standard_Fee_Configuration_Master SSFCM
+ON SFTC.Fee_Type_Id = SSFCM.Fee_Type_Id
+INNER JOIN Standard_Master SM
+ON SSFCM.Standard_Id = SM.Standard_Id
+WHERE SFTC.School_Id = 172
+AND SFTC.Academic_Year_Id = 1
+AND SFTC.Is_Deleted = 'N'
+AND SSFCM.Is_Deleted = 'N'
+AND SFTC.Fee_Type = 'Tuition Fees'
+AND SM.Is_Deleted = 'N'
+AND SM.Standard_Name IN ('1','2','3','4','5','6','7','8')
+---------------------------
+INSERT INTO StandardwiseFeeInstallmentDetails
+SELECT Standard_Id, 4, 'Installment - 1', 4620, 0,172,1,0,dbo.GetLocalDate(DEFAULT),1,dbo.GetLocalDate(DEFAULT),1
+FROM Standard_Master
+WHERE academic_Year_Id = 1
+AND School_Id = 172
+AND Is_Deleted = 'N'
+AND Is_PrePrimary = 'N'
