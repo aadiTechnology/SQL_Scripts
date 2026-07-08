c002372673512d898dd2e630ef46274ebe6c06d7
RIT Sachin <helpdesk@regulusit.net>
2026-07-08 11:52:09 +0530

    DB Changes for - PPSH - 6 Jul 2026 - 3894 - PPSH - Remove left students from current year student list All-07 Jul 2026 #3895-PPS- Day book issue for student Miss Rama Prashant Borkar (3479)

---
+++ 08-Jul-2026/01 PPSH Update_table_YearWise_Student_Details.sql
@@ -0,0 +1,48 @@
+--Rugved Pandurang bhoir
+update YearWise_Student_Details
+set Is_Deleted = 'Y'
+where YearWise_Student_Id = 28119
+and Student_Id = 785
+and Academic_Year_ID = 15
+and Is_Deleted = 'N'
+
+--Master Shabd Umesh Supekar
+update YearWise_Student_Details
+set Is_Deleted = 'Y'
+where YearWise_Student_Id = 26622
+and Student_Id = 4500
+and Academic_Year_ID = 15
+and Is_Deleted = 'N'
+
+--Master Shyam Mahadev Vaviya
+update YearWise_Student_Details
+set Is_Deleted = 'Y'
+where YearWise_Student_Id = 27712
+and Student_Id = 4434
+and Academic_Year_ID = 15
+and Is_Deleted = 'N'
+
+--Miss Jiya Sharma
+update YearWise_Student_Details
+set Is_Deleted = 'Y'
+where YearWise_Student_Id = 27288
+and Student_Id = 3584
+and Academic_Year_ID = 15
+and Is_Deleted = 'N'
+
+--Master Kushagra Verma
+update YearWise_Student_Details
+set Is_Deleted = 'Y'
+where YearWise_Student_Id = 27715
+and Student_Id = 2843
+and Academic_Year_ID = 15
+and Is_Deleted = 'N'
+
+--Miss Nishtha
+update YearWise_Student_Details
+set Is_Deleted = 'Y'
+where YearWise_Student_Id = 27885
+and Student_Id = 4186
+and Academic_Year_ID = 15
+and Is_Deleted = 'N'
+
